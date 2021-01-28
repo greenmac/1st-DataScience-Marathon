87ba872f8d2e6f596cf322089e1ba25c7be800a2
greenmac <alwaysmac@msn.com>
2021-01-28 21:37:16 +0800

    D07_Numpy_matrix作業Hong題目

--- data/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# a = np.randn()
-# print(a)
-
-a = np.array([22,87,5,43,56,73,55,54,11,20,51,5,79,31,27]) 
-print(np.sort(a))
---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# a = np.randn()
+# print(a)
+
+# a = np.array([22,87,5,43,56,73,55,54,11,20,51,5,79,31,27]) 
+# print(np.sort(a))
+
+a = np.array([ [[1, 2], [3, 4]], [[1, 2], [2, 1]], [[1, 3], [3, 1]] ])
+print(a)
